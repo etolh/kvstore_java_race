b6ea5cbbf8dec834f8134444778970d8a7175378
etolh <huliang12@126.com>
2019-07-25 09:58:51 +0800

    kv-store:fix key division problem

--- kvstore_race/src/main/java/com/huawei/hwcloud/tarus/kvstore/race/EngineKVStoreRace.java
+++ kvstore_race/src/main/java/com/huawei/hwcloud/tarus/kvstore/race/EngineKVStoreRace.java
@@ -1,7 +1,6 @@
 package com.huawei.hwcloud.tarus.kvstore.race;
 
 import com.carrotsearch.hppc.LongIntHashMap;
-import com.carrotsearch.hppc.ObjectIntHashMap;
 import com.huawei.hwcloud.tarus.kvstore.common.KVStoreRace;
 import com.huawei.hwcloud.tarus.kvstore.common.Ref;
 import com.huawei.hwcloud.tarus.kvstore.exception.KVSErrorCode;
@@ -28,6 +27,7 @@ public class EngineKVStoreRace implements KVStoreRace {
 
 	// 不同线程线程名前缀 key文件 01_kv_store.key value文件 01_1.data
 	private static final String THREAD_PATH_FORMAT = "00";
+	private static final byte base = BufferUtil.stringToBytes("0")[0];
 
 
 	// keyFile offset: 12B(400w偏移量)   valueFile: 4KB(2^16个偏移量）
@@ -43,11 +43,7 @@ public class EngineKVStoreRace implements KVStoreRace {
 
 	// 单线程写入数据
 	private static final int MSG_NUMBER = 4 * 10 ^ 6;
-	// 所有数据量
-//	private static final int ALL_MSG_NUMBER = 64 * 10^6;
 
-	// 文件存放value个数
-//	private static final int MSG_NUMBER_PERFILE = 10^6;
 	// 文件数量
 	private static final int FILE_COUNT = 64;
 	private static final int FILE_SIZE = (1 << (10 * 2 + 8)); //default size: 256MB per file
@@ -63,11 +59,7 @@ public class EngineKVStoreRace implements KVStoreRace {
 	private static AtomicInteger[] valueFileOffset = new AtomicInteger[FILE_COUNT];
 
 	// hashmap:存储key和offset的映射
-//	private static final LongLongHashMap keyOffMap = new LongLongHashMap(ALL_MSG_NUMBER, 0.99f);
 	private static final LongIntHashMap keyOffMap = new LongIntHashMap(MSG_NUMBER, 0.99f);
-	// hashmap: 存储key和value在文件中的offset的映射，由于单线程中key不重复，因此直接用key作为map的key
-	// key(string):8B valueOffset(int):4B 直接使用key，避免hash产生重复key
-//	private static final ObjectIntHashMap<String> keyOffMap = new ObjectIntHashMap(MSG_NUMBER, 0.99f);
 
 	// keyBuffer: 存储keyFile中(key,valueOff)值
 	private static FastThreadLocal<ByteBuffer> localBufferKey = new FastThreadLocal<ByteBuffer>() {
@@ -85,7 +77,7 @@ public class EngineKVStoreRace implements KVStoreRace {
 		}
 	};
 
-	//线程私有的buffer，用于byte数组转long
+	// 线程私有的buffer，用于byte数组转long
 	private static FastThreadLocal<byte[]> localValueBytes = new FastThreadLocal<byte[]>() {
 		@Override
 		protected byte[] initialValue() throws Exception {
@@ -101,6 +93,8 @@ public class EngineKVStoreRace implements KVStoreRace {
 		if (!dirParent.exists())
 			dirParent.mkdirs();
 
+
+
 		// 获取FILE_COUNT个value文件的channel
 		RandomAccessFile valueFile;
 		for (int i = 0; i < FILE_COUNT; i++) {
@@ -111,7 +105,7 @@ public class EngineKVStoreRace implements KVStoreRace {
 					vFile.createNewFile();
 				valueFile = new RandomAccessFile(vFile, "rw");
 				valueFileChannels[i] = valueFile.getChannel();
-				// 此时记录的是下一个要插入值的offset, 除相对偏移量(除去4096)或右移12位
+				// valueFileOffset[i]记录的是valueFile[i]下一个要插入值的相对offset  相对偏移量(除去4096)或右移12位
 				// valueFile.length()：字节数统计
 				valueFileOffset[i] = new AtomicInteger((int)(valueFile.length() >>> SHIF_NUM));
 			}catch (IOException e){
@@ -139,24 +133,22 @@ public class EngineKVStoreRace implements KVStoreRace {
 			ByteBuffer keyBuffer = ByteBuffer.allocate(KEY_LEN);	// 8B
 			ByteBuffer offsetBuffer = ByteBuffer.allocate(OFF_LEN);	//4B
 
-			// 记录keyFile的offset(下一个要插入的位置)：相对于1B
+			// keyFileOffset记录keyFile下一个要插入的位置的绝对offset：相对于1B
 			keyFileOffset = new AtomicInteger((int)randomKeyFile.length());
-			// index,size:相对B
 			long index = 0, size = (long) keyFileOffset.get();
 
 			while (index < size) {
-				keyBuffer.position(0);
+				keyBuffer.clear();
 				keyFileChannel.read(keyBuffer, index);
 				index += KEY_LEN;
 
-				offsetBuffer.position(0);
+				offsetBuffer.clear();
 				keyFileChannel.read(offsetBuffer, index);
 				index += OFF_LEN;
 
-				keyBuffer.position(0);
-				offsetBuffer.position(0);
+				keyBuffer.flip();
+				offsetBuffer.flip();
 				keyOffMap.put(keyBuffer.getLong(), offsetBuffer.getInt());
-//				keyOffMap.put(BufferUtil.bufferToString(keyBuffer), offsetBuffer.getInt());
 			}
 
 		} catch (IOException e) {
@@ -172,6 +164,7 @@ public class EngineKVStoreRace implements KVStoreRace {
 		byte[] keyBytes = BufferUtil.stringToBytes(key);
 		long numKey = bytes2long(keyBytes);
 		int fileNo = hash(numKey);
+		log.info("key:{} numKey:{} fileNo:{}", key, numKey, fileNo);
 
 		// valueFile offset
 		int offset = keyOffMap.getOrDefault(numKey, -1);
@@ -202,14 +195,10 @@ public class EngineKVStoreRace implements KVStoreRace {
 				keyOffBuffer.putLong(numKey).putInt(offset);
 				keyOffBuffer.flip();
 
-//				localBufferKey.get().position(0);
-//				localBufferKey.get().put(keyBytes).putInt(8, offset);	// key:string
-//	          	localBufferKey.get().putLong(numKey).putInt(offset);	// key:long
-//	          	localBufferKey.get().flip();
-
 				// 写入(key,valueoffset)到keyFile文件
 				keyFileChannel.write(keyOffBuffer, keyFileOffset.getAndAdd(KEY_OFF_LEN));
 				// 写入value到valueFile文件
+				log.info("offset:{} true:{}", offset, (long)(offset << SHIF_NUM));
 				valueFileChannels[fileNo].write(valueBuffer, (long)(offset << SHIF_NUM));
 
 				flush(keyFileChannel);
@@ -227,12 +216,14 @@ public class EngineKVStoreRace implements KVStoreRace {
 
 		long numKey = bytes2long(BufferUtil.stringToBytes(key));
 		int fileNo = hash(numKey);
+
 		// 获取map中key对应的value文件的offset
 		int offset = keyOffMap.getOrDefault(numKey, -1);
+		log.info("key:{} numKey:{} fileNo:{} offset:{}", key, numKey, fileNo, offset);
+
 		if (offset == -1) {
 			// 不存在
 			val.setValue(null);
-			throw new KVSException(KVSErrorCode.GET_RACE_ERROR, KVSErrorCode.GET_RACE_ERROR.getDescription());
 		}else {
 			// 存在
 			byte[] bytes = localValueBytes.get();
@@ -240,6 +231,7 @@ public class EngineKVStoreRace implements KVStoreRace {
 				ByteBuffer valurBuffer = localBufferValue.get();
 				// 从valueFile中读取
 				valueFileChannels[fileNo].read(valurBuffer, (long)(offset << SHIF_NUM));
+
 				valurBuffer.flip();
 				valurBuffer.get(bytes, 0, VALUE_LEN);
 				valurBuffer.clear();
@@ -289,7 +281,7 @@ public class EngineKVStoreRace implements KVStoreRace {
 	}
 
 	// key: byte[]-->long
-	public static long bytes2long(byte[] bytes) {
+	private static long bytes2long2(byte[] bytes) {
 		long result = 0;
 		for (int i = 0; i < bytes.length; i++) {
 			result <<= 8;
@@ -298,9 +290,18 @@ public class EngineKVStoreRace implements KVStoreRace {
 		return result;
 	}
 
-	// 取前6位，一共64个文件
+	private static long bytes2long(byte[] bytes) {
+		long result = 0;
+		for (int i = 0; i < bytes.length; i++) {
+			result *= 10;
+			result |= (bytes[i] - base);
+		}
+		return result;
+	}
+
+	// 64个文件,取余或0x
 	private int hash(long key) {
-		return (int)(key >>> 58);
+		return (int)(key & 0x3F);
 	}
 
 	public static final String fillThreadNo(final int no){
