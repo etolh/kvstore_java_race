138ca235987e76fdfc9db0296e39a57f6536972d
etolh <huliang12@126.com>
2019-07-29 09:18:18 +0800

    按插入顺序分区成功 10000

---
+++ kvstore_race/src/main/java/com/huawei/hwcloud/tarus/kvstore/race/DataManager.java
@@ -0,0 +1,11 @@
+package com.huawei.hwcloud.tarus.kvstore.race;
+
+/**
+ * @ClassName DataManager
+ * @Description TODO
+ * @Author lianghu
+ * @Date 2019-07-28 22:58
+ * @VERSION 1.0
+ */
+public class DataManager {
+}
